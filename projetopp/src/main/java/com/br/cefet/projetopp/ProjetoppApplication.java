b7cb65c743728a3ca1e76c44064bf48eec8e3943
Jvmcst <jv11102014@gmail.com>
2023-08-13 16:26:41 -0300

    Feito

---
+++ projetopp/src/main/java/com/br/cefet/projetopp/ProjetoppApplication.java
@@ -0,0 +1,13 @@
+package com.br.cefet.projetopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoppApplication.class, args);
+	}
+
+}
---
+++ projetopp/src/test/java/com/br/cefet/projetopp/ProjetoppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cefet.projetopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
