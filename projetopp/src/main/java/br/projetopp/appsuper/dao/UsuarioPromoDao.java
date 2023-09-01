c3102abad79c8a77b3c86b603cbe8d8a982c618d
Jvmcst <jv11102014@gmail.com>
2023-09-01 07:27:34 -0300

    up0

---
+++ projetopp/src/main/java/br/projetopp/appsuper/dao/UsuarioPromoDao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.projetopp.appsuper.dao;
+
+import br.projetopp.appsuper.model.Promocao;
+import br.projetopp.appsuper.model.UsuarioPromo;
+import java.util.List;
+
+/**
+ *
+ * @author jv111
+ */
+public class UsuarioPromoDao {
+
+    public void insert(UsuarioPromo usuarioPromo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public List<Promocao> getByUsuario(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void delete(int idUsuario, int idPromocao) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void deleteByUsuario(int idUsuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
